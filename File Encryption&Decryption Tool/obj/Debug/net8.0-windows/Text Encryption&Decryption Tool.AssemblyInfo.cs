125328c78b2f0e461ed0ead83edda8e6875b779b
Pierre Joubert <joubertpierre96@gmail.com>
2024-08-27 07:50:09 +0200

    Repository cloned.

--- File Encryption&Decryption Tool/obj/Debug/net8.0-windows/Text Encryption&Decryption Tool.AssemblyInfo.cs
+++ File Encryption&Decryption Tool/obj/Debug/net8.0-windows/Text Encryption&Decryption Tool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Text Encryption&Decryption Tool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5195e2355928f71608e1119d9b85eba1667af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Text Encryption&Decryption Tool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Text Encryption&Decryption Tool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
